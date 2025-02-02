f06c99f35ca44e0b9416632049e3b2a86a65ca86
VBondB <Bvv051270@gmail.com>
2025-02-02 22:07:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Введіть число: ");
+string input = Console.ReadLine();
+
+try
+{
+    int number = int.Parse(input);
+    Console.WriteLine($"Ви ввели число: {number}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Помилка: введене значення не є числом.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Сталася помилка: {ex.Message}");
+}
